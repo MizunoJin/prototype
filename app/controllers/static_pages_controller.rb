85d3ac5c14bd0a86cdbca698a41f167bd928937e
mizuno <jin.mizuno@eisansystem.jp>
2021-07-12 17:48:15 +0900

    Save click data to database

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,18 @@
 class StaticPagesController < ApplicationController
   def index
+    @coordinate = Coordinate.all
   end
 
   def sample
   end
 
   def save
-    @data = params[:data]
-    debugger
+    coordinate = Coordinate.new(coordinate_params)
+    coordinate.save!
+  end
+
+  private
+  def coordinate_params
+    params.permit(:x, :y, :url)
   end
 end
